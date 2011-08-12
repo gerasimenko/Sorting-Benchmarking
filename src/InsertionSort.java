9cf3d2a22546c4a6ec027b935b410fe914ad904d
gerasimenko <mgerasimenko@.ARTGROUP.local>
2011-08-12 16:12:46 +0400

    first commit

---
+++ src/InsertionSort.java
@@ -0,0 +1,18 @@
+/**
+ * Created by IntelliJ IDEA.
+ * User: mgerasimenko
+ * Date: 8/12/11
+ * Time: 2:13 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class InsertionSort {
+
+    public long[] sort(long[] array) {
+
+        return array;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/RandomArray.java
@@ -0,0 +1,44 @@
+import java.util.Date;
+import java.util.Random;
+
+public class RandomArray {
+    Random randomGenerator;
+    private int length;
+    private long range;
+    private long[] arrayContainer = null;
+
+    public RandomArray(int length, long range) {
+        this.length = length;
+        this.range = range;
+        randomGenerator = new Random(new Date().getTime());
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public long getRange() {
+        return range;
+    }
+
+    public long[] get() {
+        if (arrayContainer == null) {
+            arrayContainer = new long[this.length];
+            for (int i = 0; i < arrayContainer.length; i++) {
+                arrayContainer[i] = Math.abs(randomGenerator.nextLong()) % range;
+            }
+        }
+
+        return arrayContainer;
+    }
+
+    public String getStringPresentation() {
+        get();
+        String result = "[";
+        for (int i = 0; i < arrayContainer.length; i++) {
+            if (i > 0) result += ", ";
+            result += arrayContainer[i];
+        }
+        return result + "]";
+    }
+}
